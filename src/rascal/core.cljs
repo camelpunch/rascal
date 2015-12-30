d8ecd5ee029cc7452b88a61e603127d63378be0d
Andrew Bruce <me@andrewbruce.net>
2015-12-30 12:24:01 +0000

    Show health of monsters
    Also, refactor core.cljs a bit

--- src/rascal/core.cljs
+++ src/rascal/core.cljs
@@ -4,12 +4,12 @@
 
 (enable-console-print!)
 
-(def jackal
+(def new-jackal
   {:tile   \j
    :name   "Jackal"
    :health 100})
 
-(def kobold
+(def new-kobold
   {:tile   \k
    :name   "Kobold"
    :health 100})
@@ -21,8 +21,8 @@
 (def state (r/atom {:board (->Board 15 15)
                     :player {:tile   \@
                              :coords {:x 7 :y 7}}
-                    :monsters [(-> jackal (at [13 10]))
-                               (-> kobold (at [ 1  1]))]}))
+                    :monsters [(-> new-jackal (at [13 10]))
+                               (-> new-kobold (at [ 1  1]))]}))
 
 (def keymap
   {72 b/move-left
@@ -35,6 +35,15 @@
   (when-let [f (keymap (-> e .-keyCode))]
     (swap! state f)))
 
+(defn game-cell
+  [idx contents]
+  [:td.cell {:key (str "td" idx)} contents])
+
+(defn game-row
+  [idx contents]
+  [:tr {:key (str "tr" idx)}
+   (map-indexed game-cell contents)])
+
 (defn main
   []
   [:div#game.invisibleFocus.textC
@@ -42,16 +51,12 @@
     :on-key-down keydown-handler}
    [:h1 "Rascal"]
    [:table.marginC
-    [:tbody
-     (map-indexed
-      (fn [row-idx row]
-        [:tr {:key (str "tr" row-idx)}
-         (map-indexed
-          (fn [cell-idx cell]
-            [:td.cell {:key (str "td" cell-idx)}
-             cell])
-          row)])
-      (b/render @state))]]])
+    [:tbody (map-indexed game-row (b/render @state))]]
+   [:ul
+    (for [monster (:monsters @state)]
+      [:li.item1of3
+       [:h2 (:name monster)]
+       [:p "Health: " (:health monster)]])]])
 
 (def main-focused
   (-> main (with-meta {:component-did-mount
