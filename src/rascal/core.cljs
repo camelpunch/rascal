b9edf181978125e05693370989fd4d666fe0ff6f
Andrew Bruce <me@andrewbruce.net>
2016-01-02 23:11:26 +0000

    Some UX improvements
    - Show player health
    - Keys help

--- src/rascal/core.cljs
+++ src/rascal/core.cljs
@@ -7,7 +7,7 @@
 (enable-console-print!)
 
 (def state (r/atom {:board    (t/make-board                 30 25)
-                    :player   (t/make-player                 7  7)
+                    :player   (t/make-player                15 23)
                     :monsters [(t/make-creature \j "Jackal" 13 10)
                                (t/make-creature \r "Rat"     1  1)]}))
 
@@ -36,19 +36,26 @@
   [:div#game.page.invisibleFocus
    {:tab-index 0
     :on-key-down keydown-handler}
-   [:div.textC.header.break
-    [:h1.mainHeading "Rascal"]
-    [:p [:a {:href "https://github.com/camelpunch/rascal"} "Source"]]]
+   [:div.break.yui3-g
+    [:div.yui3-u-1-3
+     [:h2.bld "Player"]
+     [:p "Health: " (get-in @state [:player :health])]]
+    [:div.yui3-u-1-3.textC
+     [:h1.mainHeading "Rascal"]
+     [:p [:a {:href "https://github.com/camelpunch/rascal"} "Source"]]]
+    [:div.yui3-u-1-3]]
    [:div.yu3-g
     [:div.yui3-u-1-4
      [:ul.monsters
      (for [monster (:monsters @state)]
        [:li.monster {:key (str monster)}
-        [:h2.monster-name (:name monster)]
+        [:h2.bld (:name monster)]
         [:p.monster-detail.break "Health: " (:health monster)]])]]
     [:div.board.yui3-u-3-4
-     [:table.textC.marginC
-      [:tbody (map-indexed game-row (render @state))]]]]])
+     [:table.textC.break
+      [:tbody (map-indexed game-row (render @state))]]
+     [:h2.bld "Keys:"]
+     [:p "h,j,k,l - movement"]]]])
 
 (def main-focused
   (-> main (with-meta {:component-did-mount
