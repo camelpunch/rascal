c4b88cd43d94cf651c815933f471c22264aab8b4
Andrew Bruce <me@andrewbruce.net>
2016-01-03 00:11:56 +0000

    Some test helpers to render boards in the REPL

--- test/rascal/test_helpers.clj
+++ test/rascal/test_helpers.clj
@@ -1,5 +1,18 @@
-(ns rascal.test-helpers)
+(ns rascal.test-helpers
+  (:require [rascal.tiles :as tiles]
+            [rascal.render :refer [render]]))
+
+(def g
+  {:board (tiles/make-board 10 10)
+   :player (tiles/make-player 1 1)
+   :monsters [(tiles/make-creature \j "Jackal" 1 2)]})
 
 (defn rendered
   [b]
   (map vec (clojure.string/split b #"\n +")))
+
+(defn game-s
+  [g]
+  (clojure.string/join "\n" (map (partial apply str) (render g))))
+
+(def pb (comp println game-s))
