06e8f6bc5c0d87e23d51983391e94750bc904946
Andrew Bruce <me@andrewbruce.net>
2016-01-12 21:01:44 +0000

    Oops, update tests

--- test/rascal/game_test.clj
+++ test/rascal/game_test.clj
@@ -1,7 +1,7 @@
 (ns rascal.game-test
   (:use [clojure.test])
   (:require [rascal.test-helpers :refer [rendered]]
-            [rascal.game :as game]
+            [rascal.game :as game :refer [move left]]
             [rascal.tiles :refer [x-axis
                                   y-axis
                                   make-board
@@ -25,41 +25,40 @@
       (is (= [0 10]
              (:dice-rolls game-start))))))
 
-(deftest move
+(deftest movement
   (testing "normal movement moves player"
     (is (= 4
            (get-in (-> (game/make-game :player [5 1]
                                        :board  [6 6])
-                       (game/move x-axis dec))
+                       (move left))
                    [:player :coords :x]))))
   (testing "into wall"
     (is (= 1
            (get-in (-> (game/make-game :player [1 1]
                                        :board  [3 3])
-                       (game/move x-axis dec))
+                       (move left))
                    [:player :coords :x])))))
 
 (deftest collision
   (let [game-start (game/make-game :board      [ 8 10]
                                    :player     [ 5  2]
                                    :monsters   [[\j "Jackal"] [\b "Beetle"]]
-                                   :dice-rolls [ 5  2
-                                                10  3])
-        go-left #(game/move % x-axis dec)]
+                                   :dice-rolls [ 5  2 ; jackal at [4 2]
+                                                10  3])] ; beetle at [8 3]
 
     (testing "logs the fight"
       (is (= ["You entered the dungeon"
               "You hit the Jackal"
               "You defeated the Jackal"]
              (-> game-start
-                 go-left
-                 go-left
+                 (move left)
+                 (move left)
                  :log))))
 
     (testing "player doesn't move"
       (is (= {:x 5 :y 2}
              (-> game-start
-                 go-left
+                 (move left)
                  :player
                  :coords))))))
 
