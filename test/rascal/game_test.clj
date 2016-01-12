1b5d292eff3ac78fd5d27fbe47dd892a1861b80e
Andrew Bruce <me@andrewbruce.net>
2016-01-12 21:04:04 +0000

    Tidy game_test.clj

--- test/rascal/game_test.clj
+++ test/rascal/game_test.clj
@@ -1,7 +1,7 @@
 (ns rascal.game-test
   (:use [clojure.test])
   (:require [rascal.test-helpers :refer [rendered]]
-            [rascal.game :as game :refer [move left]]
+            [rascal.game :refer [make-game move left]]
             [rascal.tiles :refer [x-axis
                                   y-axis
                                   make-board
@@ -12,12 +12,12 @@
             [rascal.render :refer [render]]))
 
 (deftest creating-a-game
-  (let [game-start (game/make-game :board      [ 8 10]
-                                   :player     [ 5  2]
-                                   :monsters   [[\j "Jackal"] [\b "Beetle"]]
-                                   :dice-rolls [ 5  2
-                                                10  3
-                                                 0 10])]
+  (let [game-start (make-game :board      [ 8 10]
+                              :player     [ 5  2]
+                              :monsters   [[\j "Jackal"] [\b "Beetle"]]
+                              :dice-rolls [ 5  2
+                                           10  3
+                                           0 10])]
     (testing "starts on turn 1"
       (is (= 1 (:turn game-start))))
 
@@ -28,23 +28,23 @@
 (deftest movement
   (testing "normal movement moves player"
     (is (= 4
-           (get-in (-> (game/make-game :player [5 1]
-                                       :board  [6 6])
+           (get-in (-> (make-game :player [5 1]
+                                  :board  [6 6])
                        (move left))
                    [:player :coords :x]))))
   (testing "into wall"
     (is (= 1
-           (get-in (-> (game/make-game :player [1 1]
-                                       :board  [3 3])
+           (get-in (-> (make-game :player [1 1]
+                                  :board  [3 3])
                        (move left))
                    [:player :coords :x])))))
 
 (deftest collision
-  (let [game-start (game/make-game :board      [ 8 10]
-                                   :player     [ 5  2]
-                                   :monsters   [[\j "Jackal"] [\b "Beetle"]]
-                                   :dice-rolls [ 5  2 ; jackal at [4 2]
-                                                10  3])] ; beetle at [8 3]
+  (let [game-start (make-game :board      [ 8 10]
+                              :player     [ 5  2]
+                              :monsters   [[\j "Jackal"] [\b "Beetle"]]
+                              :dice-rolls [ 5  2    ; Jackal at [4 2]
+                                           10  3])] ; Beetle at [8 3]
 
     (testing "logs the fight"
       (is (= ["You entered the dungeon"
